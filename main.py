a14348edf2f814dae98937d889fe289814408ded
Johnson <tjtgzxs@gmail.com>
2022-10-10 20:11:33 +0800

    add conf

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import Constant
 import configparser
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    upload=upload()
-    result=upload.process("tjtgjohnson","1990lljxk",Constant.BASE_DIR+"1.mp4")
+    config = configparser.ConfigParser()
+    config.read(Constant.BASE_DIR+"conf.ini")
+    video_dir=config.get("DEFAULT","video_path")
+    upload=upload(config.getboolean("WEBDRIVE","headless"))
+    result=upload.process(config.get("DEFAULT","user_name"),config.get("DEFAULT",'password'),Constant.BASE_DIR+"1.mp4")
     print(result)
     upload.close()
 
--- upload.py
+++ upload.py
@@ -16,7 +16,7 @@ from send import main as send_msg
 from send import send_img
 
 class upload:
-    def __init__(self):
+    def __init__(self,headless=True):
         try:
             options = ChromeOptions()
             mimvp_proxy = {
@@ -35,7 +35,7 @@ class upload:
             options.add_argument(proxy_socks_argument)
             self.browser = uc.Chrome(browser_executable_path=Constant.BROWSER_EXECUTABLE_PATH,
                                  driver_executable_path=Constant.DRIVE_EXECUTABLE_PATH,
-                                 options=options,headless=True)
+                                 options=options,headless=headless)
             logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
             self.logger = logging.getLogger(__name__)
             self.logger.setLevel(logging.INFO)
